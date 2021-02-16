4c13062d2244eb966295a477e3db85162701fb39
William Zimmermann <wzimmermann@MacBook-Pro-de-William.local>
2021-02-16 17:59:46 -0500

    End of tests for lesson 4

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,6 +1,8 @@
-import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { Component, Input, NO_ERRORS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 import { of } from "rxjs";
+import { Hero } from "../hero";
 import { HeroService } from "../hero.service";
 import { HeroesComponent } from "./heroes.component";
 
@@ -8,6 +10,14 @@ describe("HeroesComponent (Shallow test)", () => {
     let fixture: ComponentFixture<HeroesComponent>;
     let mockHeroService;
     let HEROES;
+    @Component({
+        selector: 'app-hero',
+        template: '<div></div>',
+    })
+    class FakeHeroComponent {
+        @Input() hero: Hero;
+        // @Output() delete = new EventEmitter();
+    }
 
     beforeEach(()=>{
         HEROES = [
@@ -23,14 +33,16 @@ describe("HeroesComponent (Shallow test)", () => {
         ]);
 
         TestBed.configureTestingModule({
-            declarations: [HeroesComponent],
+            declarations: [
+                HeroesComponent,
+                FakeHeroComponent
+            ],
             providers: [
                 { 
                     provide: HeroService,
                     useValue: mockHeroService
                 }
             ],
-            schemas: [NO_ERRORS_SCHEMA]
         });
 
         fixture = TestBed.createComponent(HeroesComponent);
@@ -41,5 +53,12 @@ describe("HeroesComponent (Shallow test)", () => {
         fixture.detectChanges();
 
         expect(fixture.componentInstance.heroes.length).toBe(3);
+    });
+
+    it("should create one li for each hero", () => {
+        mockHeroService.getHeroes.and.returnValue(of(HEROES));
+        fixture.detectChanges();
+
+        expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3);
     });
 });
