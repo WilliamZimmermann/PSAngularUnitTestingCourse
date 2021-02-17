65ee4710ddcb1f4ca3d21de7834546d029ed89a4
William Zimmermann <wzimmermann@MacBook-Pro-de-William.local>
2021-02-17 09:55:45 -0500

    Test Bed. Lesson 5

---
+++ src/app/hero.service.spec.ts
@@ -0,0 +1,39 @@
+import { inject, TestBed } from "@angular/core/testing";
+import { HeroService } from "./hero.service";
+import { MessageService } from "./message.service";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+describe("HeroService", () => {
+
+    let mockMessageService;
+    let httpTestingController: HttpTestingController;
+    let service;
+
+    beforeEach(()=>{
+        mockMessageService = jasmine.createSpyObj(['add']);
+
+        TestBed.configureTestingModule({
+            imports: [ HttpClientTestingModule ],
+            providers: [
+                HeroService,
+                { 
+                    provide: MessageService,
+                    useValue: mockMessageService
+                }
+            ]
+        });
+
+        httpTestingController = TestBed.inject(HttpTestingController);
+        service = TestBed.inject(HeroService);
+    });
+
+    describe('hetHero', () => {
+
+        it('should call get with the correct URL', () => {
+            service.getHero(4).subscribe();
+
+            const request = httpTestingController.expectOne('api/heroes/4');
+            request.flush({id: 4, name: 'SuperDude', strength: 100});
+
+        });
+    });
+});
